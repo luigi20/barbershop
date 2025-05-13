94a6c04b3977c6f94ad15474786c08d51ea89cbf
Luis <luisopentec@gmail.com>
2025-05-13 14:59:54 -0300

    initial commit

--- src/infra/redis/service/redis.service.ts
+++ src/infra/redis/service/redis.service.ts
@@ -5,7 +5,7 @@ import Redis from 'ioredis';
 @Injectable()
 export class RedisService implements OnModuleDestroy {
   private client: Redis;
-
+  /*
   constructor() {
     this.client = new Redis({
       host: process.env.REDIS_HOST,
@@ -18,7 +18,7 @@ export class RedisService implements OnModuleDestroy {
   getClient(): Redis {
     return this.client;
   }
-
+*/
   async onModuleDestroy() {
     await this.client.quit();
   }
